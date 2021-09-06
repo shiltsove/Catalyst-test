84e1364a1d907af22b8fa14012e522d6c052049a
Jack Shiltsov <shiltsove@gmail.com>
2021-09-06 13:56:19 +1200

    adding new model for questionnare

--- cloud_test_app/questionnaire/models.py
+++ cloud_test_app/questionnaire/models.py
@@ -1,1 +1,19 @@
+import calendar
 from django.db import models
+
+
+class Questionnare(models.Model):
+    DAY_CHOICES = [
+        (1, "Monday"),
+        (2, "Tuesday"),
+        (3, "Wednesday"),
+        (4, "Thursday"),
+        (5, "Friday"),
+        (6, "Saturday"),
+        (7, "Sunday")
+    ]
+
+    MONTH_CHOICES = [(value, calendar.month_name[value]) for value in range(1, 13)]
+
+    month = models.IntegerField(choices=MONTH_CHOICES)
+    day = models.IntegerField(choices=DAY_CHOICES)
